510e241ecd83aefd3f1e00fd9b53107af2f2ac31
Darm94 <94damianos@gmail.com>
2025-06-25 13:56:14 +0200

    fi: button reload button

--- Assets/SceneReloader.cs
+++ Assets/SceneReloader.cs
@@ -6,6 +6,7 @@ public class SceneReloader : MonoBehaviour
     // Metodo pubblico per ricaricare la scena corrente
     public void ReloadCurrentScene()
     {
+        Time.timeScale = 1f;
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
     }
