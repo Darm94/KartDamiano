790547f327394c1f96c1293f79b050e3adbc5bd6
Darm94 <94damianos@gmail.com>
2025-06-25 13:52:28 +0200

    fix: aggiunto button dimenticato

---
+++ Assets/SceneReloader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReloader : MonoBehaviour
+{
+    // Metodo pubblico per ricaricare la scena corrente
+    public void ReloadCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+}
